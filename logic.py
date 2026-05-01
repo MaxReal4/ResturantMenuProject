bf6c7d60d8018bde9c3a125991ec92ccce603c05
MaxReal4 <77829604@nebraska.edu>
2026-05-01 12:21:03 -0500

    Final commit?

--- logic.py
+++ logic.py
@@ -4,12 +4,13 @@ from PyQt6.QtWidgets import *
 from menu import *
 import csv
 
-# DO I NEED DOCK STRINGS!@!!!
+
 class Logic(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
 
+        #sets a fixed size of the menu
         self.setFixedSize(500, 425)
 
 
@@ -18,7 +19,8 @@ class Logic(QMainWindow, Ui_MainWindow):
         self.SendButton.clicked.connect(lambda: self.submit())
 
     def submit(self):
-        tablenumber = self.TableBox.text().strip()
+        tablenumber: str = self.TableBox.text().strip()
+
         if tablenumber == '':
             self.ThanksLabel.setText('You need a table number!')
             return
@@ -47,8 +49,14 @@ class Logic(QMainWindow, Ui_MainWindow):
                 "MapleBoba": self.MapleBobaBox
             }
         }
-
-        #I used AI here until csv file
+        '''
+        nested for loop taking each catagory raw,cooked... then taking the items in those
+        catagorys like cali roll then grabing the amount listed in that dictionary. What it 
+        then does is strip the value then sees if it is a digit because if not it wont print
+        it and moves on. If it is a digit then it adds it into the list of rows in the order
+        table number, catagory, name, quantity.
+        '''
+        #I used AI here until csv file writer
         for catagory, items in whole_menu.items():
             for name, amount in items.items():
                 value = amount.text().strip()
@@ -64,13 +72,20 @@ class Logic(QMainWindow, Ui_MainWindow):
 
                     ])
 
-
+        """
+        opening of the csv file and writes each row in the list rows which should be
+        Table number, catagory/type of food, name of food/drink, quantity.
+        """
         with open('ResturantTickets.csv', 'a', newline='') as csvfile:
             writer = csv.writer(csvfile)
 
             for row in rows:
-                writer.writerow(row)
+                writer.writerow(row) #writes each row in the list rows
 
+
+        '''
+        clears all the boxes on the menu
+        '''
         self.TunaBox.clear()
         self.MahiBox.clear()
         self.SalmonBox.clear()
